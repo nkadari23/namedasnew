5e730081ce77b69501fa5a8315994abf0d563217
nkadari23 <vk.nashrinkadri@gmail.com>
2024-10-20 13:27:39 +0300

    code slighlty changed

---
+++ folder1/simplifier.py
@@ -0,0 +1,21 @@
+# Simple math operations in Python
+a = 10
+b = 5
+
+# Addition
+add = a + b
+print("Addition:", add)
+
+# Subtraction
+sub = a - b
+print("Subtraction:", sub)
+
+# Multiplication
+mul = a * b
+print("Multiplication:", mul)
+
+# Division
+div = a / b
+print("Division:", div)
+
+print("Math operations completed successfully!")
